a97e34201203fea4e7436a0a18cf1634f9288f05
estherceciliaS <esther.oliveira@natahouse.com>
2022-11-07 10:15:59 -0300

    fix: change color default

--- src/constants/defaultProfile.js
+++ src/constants/defaultProfile.js
@@ -3,7 +3,7 @@ export const defaultStyle = {
   id: "3082492f-5d87-48c4-8152-9ba37d829cd5",
   logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQdc_pViMeba37-qxWAWLBm1Bn5XeQWZoadWA&usqp=CAU",
   colors: {
-    primaryColor: "#2E331F",
+    primaryColor: "#bc7c4b",
     secondaryColor: "#363537",
   },
 };
