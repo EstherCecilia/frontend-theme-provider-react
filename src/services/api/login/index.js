57e94fdcb59e8a57ff479f9177b08fe26e91d3b1
estherceciliaS <esther.oliveira@natahouse.com>
2022-11-07 10:33:27 -0300

    fix: login

--- src/services/api/login/index.js
+++ src/services/api/login/index.js
@@ -5,28 +5,20 @@ export const login = async (filters) => {
 
   let response;
 
-  if (filters.email === "admin") {
-    if (filters.password === "admin") {
-      response = {
-        status: true,
-        data: {
-          name: "Luciana Veiga",
-          user: "Luciana",
-          email: "luciana@gmail.com",
-          permissao: "Analista",
-          refresh:
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY0NjE2MDkzOCwiaWF0IjoxNjM4Mzg0OTM4LCJqdGkiOiI4NjMxZDEyY2VkZTY0OTM1OTEyNmY5N2E0ZDUyOTM3OCIsInVzZXJfaWQiOjJ9.hKTLm07her-pW4eMpczD4MfLROrXGikUF3s6-gkm2fc",
-          access:
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjM4NDcxMzM4LCJpYXQiOjE2MzgzODQ5MzgsImp0aSI6ImJjYmU1MTFmOTg1OTRmNjZiYjAyZDMyNTAyYmZhNjAzIiwidXNlcl9pZCI6Mn0.gmVMjSesQH8eU7AL9Y2cgJm5IKoOSOP0DF67kLuMS_Q",
-        },
-      };
-    } else {
-      response = {
-        status: false,
-        error: "senha incorreta!",
-        data: undefined,
-      };
-    }
+  if (filters.email && filters.password) {
+    response = {
+      status: true,
+      data: {
+        name: "Luciana Veiga",
+        user: "Luciana",
+        email: "luciana@gmail.com",
+        permissao: "Analista",
+        refresh:
+          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY0NjE2MDkzOCwiaWF0IjoxNjM4Mzg0OTM4LCJqdGkiOiI4NjMxZDEyY2VkZTY0OTM1OTEyNmY5N2E0ZDUyOTM3OCIsInVzZXJfaWQiOjJ9.hKTLm07her-pW4eMpczD4MfLROrXGikUF3s6-gkm2fc",
+        access:
+          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjM4NDcxMzM4LCJpYXQiOjE2MzgzODQ5MzgsImp0aSI6ImJjYmU1MTFmOTg1OTRmNjZiYjAyZDMyNTAyYmZhNjAzIiwidXNlcl9pZCI6Mn0.gmVMjSesQH8eU7AL9Y2cgJm5IKoOSOP0DF67kLuMS_Q",
+      },
+    };
   } else {
     response = {
       status: false,
